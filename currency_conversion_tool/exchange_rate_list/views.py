12053b827a740b036fb2e91665bafe6c913f7c73
Sterling Sutton <113621387+sterlingsutton@users.noreply.github.com>
2024-11-20 17:27:36 +0000

    Fix exchange rates list

--- currency_conversion_tool/exchange_rate_list/views.py
+++ currency_conversion_tool/exchange_rate_list/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 import requests
 
 # Create your views here.
 def exchange_rate_list(request):
-    rates = requests.get('https://api.frankfurter.app/latest')
-    return HttpResponse(rates)
+    template = loader.get_template('exchange_rate_list.html')
+    rates = requests.get('https://api.frankfurter.app/latest').json()['rates']
+    context = {
+        'rates': rates
+    }
+    return HttpResponse(template.render(context, request))
