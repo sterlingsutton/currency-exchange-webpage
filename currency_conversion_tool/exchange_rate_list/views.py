c7fafec1479fe65e4f57955df565216c336f0de4
Sterling Sutton <113621387+sterlingsutton@users.noreply.github.com>
2024-11-19 22:51:29 -0800

    Test ability to make API calls and print them

--- currency_conversion_tool/exchange_rate_list/views.py
+++ currency_conversion_tool/exchange_rate_list/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse
+import requests
 
 # Create your views here.
 def exchange_rate_list(request):
-    return HttpResponse("Hello, world!")
+    rates = requests.get('https://api.frankfurter.app/latest')
+    return HttpResponse(rates)
