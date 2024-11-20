c1bd25c49758ed45d391529e0fc09fc0bf97351d
Sterling Sutton <113621387+sterlingsutton@users.noreply.github.com>
2024-11-19 22:36:56 -0800

    Fix some errors with Django

--- currency_conversion_tool/exchange_rate_list/views.py
+++ currency_conversion_tool/exchange_rate_list/views.py
@@ -2,7 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
 # Create your views here.
-def exchange_rate_list():
-    #req = HttpRequest('https://api.frankfurter.app/latest')
-    #HttpResponse(req['rates'])
-    HttpResponse("Hello, world!")
+def exchange_rate_list(request):
+    return HttpResponse("Hello, world!")
